385ae0b244c79313ce986ff6a9a49b2b4bc7e2f7
Samuel Bruce <sbruce1@firstderivatives.com>
2022-05-17 11:33:54 +1000

    Modifying startup script to be more robust across machines

--- startup.sh
+++ startup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-export BASE_DIRECTORY="/home/sbruce1/Desktop/Work/Projects/Git/ICE/kdb-tick"
-export ON_DISK_HDB="/home/sbruce1/Desktop/Work/Projects/Git/ICE/kdb-tick/OnDiskDB/"
-export TICK_DIRECTORY="/home/sbruce1/Desktop/Work/Projects/Git/ICE/kdb-tick/tick/"
+
+BASE_DIRECTORY=$(dirname "$0")
+if [ ! -d $BASE_DIRECTORY/OnDiskDB ]
+then
+    echo "Creating On Disk DB ${BASE_DIRECTORY}/OnDiskDB"
+    mkdir -p ${BASE_DIRECTORY}/OnDiskDB
+fi
+ON_DISK_HDB=${BASE_DIRECTORY}/OnDiskDB
+TICK_DIRECTORY=${BASE_DIRECTORY}/tick
+# WORKING_DIRECTORY=${PWD}
 
 cd $BASE_DIRECTORY
 q tick.q sym $ON_DISK_HDB -p 5000 &
@@ -10,5 +17,5 @@ q hdb.q $ON_DISK_HDB -p 5002 &
 cd $TICK_DIRECTORY
 q r.q localhost:5000 localhost:5002 -p 5008 &
 
-#cd $BASE_DIRECTORY
-#q feedhandler.q -p 5008 
+# #cd $BASE_DIRECTORY
+# #q feedhandler.q -p 5008 
