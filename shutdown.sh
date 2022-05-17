4fb5d14693861c04c8a5caf015af70599e2969f2
Samuel Bruce <sbruce1@firstderivatives.com>
2022-05-17 16:59:50 +1000

    Adding FH to shutdown

--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,4 @@
 ps aux | grep tick.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
 ps aux | grep hdb.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
 ps aux | grep r.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep feedhandler_gda.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
