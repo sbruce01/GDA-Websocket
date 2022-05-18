55e4354c34b8ffb749e79bc2b4601eba1efe15fb
Samuel Bruce <sbruce1@firstderivatives.com>
2022-05-18 16:31:43 +1000

    Adding CTP to shutdown script

--- shutdown.sh
+++ shutdown.sh
@@ -3,3 +3,4 @@ ps aux | grep hdb.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
 ps aux | grep r.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
 ps aux | grep feedhandler_gda.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
 ps aux | grep chainedr.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep wschaintick_0.2.2.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
