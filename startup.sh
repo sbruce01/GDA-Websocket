3e20a79b0b2965b4bcebfed32e429c44814a5a52
Samuel Bruce <sbruce1@firstderivatives.com>
2022-05-18 11:16:58 +1000

    Changing HDB directory

--- startup.sh
+++ startup.sh
@@ -7,17 +7,15 @@ then
     mkdir -p ${BASE_DIRECTORY}/OnDiskDB
 fi
 ON_DISK_HDB=${BASE_DIRECTORY}/OnDiskDB/
+HDB_STARTUP_DIR=${ON_DISK_HDB}/sym/
 TICK_DIRECTORY=${BASE_DIRECTORY}/tick/
 
 cd $BASE_DIRECTORY
 q tick.q sym $ON_DISK_HDB -p 5000 &
-q hdb.q $ON_DISK_HDB -p 5002 &
+q hdb.q $HDB_STARTUP_DIR -p 5002 &
 
 cd $TICK_DIRECTORY
 q r.q localhost:5000 localhost:5002 -p 5008 &
 
 cd $BASE_DIRECTORY
 q feedhandler_gda.q -p 5111 > allExchanges.log 2>&1 &
-
-# #cd $BASE_DIRECTORY
-# #q feedhandler.q -p 5008 
