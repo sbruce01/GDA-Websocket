919ff56752a21aee5d0c73a92ebcd7e0187a32a8
Samuel Bruce <sbruce1@firstderivatives.com>
2022-05-19 15:58:41 +1000

    Adding space in shutdown to kill correct processes

--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,7 @@
-ps aux | grep tick.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
-ps aux | grep hdb.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
-ps aux | grep r.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
-ps aux | grep feedhandler_gda.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
-ps aux | grep chainedr.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
-ps aux | grep wschaintick_0.2.2.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
-ps aux | grep gw.q | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' tick.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' hdb.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' r.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' feedhandler_gda.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' chainedr.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' wschaintick_0.2.2.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps aux | grep ' gw.q' | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
