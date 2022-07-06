df51aeef8f6b809ac3df428905467ad395d262fb
Samuel Bruce <sbruce1@firstderivatives.com>
2022-07-06 17:24:11 +1000

    Moving to Apollo GraphQL

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,8 +1,41 @@
-const { createServer } = require('graphql-yoga')
+const { ApolloServer, gql } = require('apollo-server');
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const baseURL = `http://localhost:5005`
+
+function getFilters(info) {
+    user_functions = info.operation.selectionSet.selections
+        for (let i = 0; i < user_functions.length; i++){
+            if (user_functions[i].name.value===info.fieldName) {
+                for (let j = 0; j < user_functions[i].selectionSet.selections.length; j++){
+                    console.log(user_functions[i].selectionSet.selections[j].name.value)
+                }                
+            }
+        }
+  }
+
+const typeDefs = gql`
+    type Query {
+        data: [Trade!]!
+        dataWithStartTime(startTime: String!): [Trade!]!
+        dataWithMultiple(startTime: String!, exchange: [String!]): [Trade!]!
+        logHeader:[Trade!]!
+        allInfo:[Trade!]!
+    }
+
+    type Trade {
+        date: String!
+        time: String!
+        sym: String!
+        orderID: String!
+        price: Float!
+        tradeID: String!
+        side: String!
+        size: Float!
+        exchange: String!
+    }
+`;
 
 const resolvers = {
     Query: {
@@ -17,32 +50,16 @@ const resolvers = {
         const { startTime, exchange } = args
         return fetch(`${baseURL}/getData?exc=${exchange}&sd=${startTime}`).then(res => res.json())
       },
+      allInfo: (_, args, context, info) => {
+        getFilters(info)
+        return [{tradeId: "yes"}]
+    }
     },
   }
 
-const server = createServer({
-    schema: {
-      typeDefs: `
-        type Query {
-            data: [Data!]!
-            dataWithStartTime(startTime: String!): [Data!]!
-            dataWithMultiple(startTime: String!, exchange: [String!]): [Data!]!
-        }
-        
-        type Data {
-            date: String!
-            time: String!
-            sym: String!
-            orderID: String!
-            price: Float!
-            tradeID: String!
-            side: String!
-            size: Float!
-            exchange: String!
-        }
-      `,
-      resolvers: resolvers,
-      },
-  })
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
